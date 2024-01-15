35f1279da65847c020347374afb4e501d0d29f5e
Pavandkurdekar <pavandkurdekar55@gmail.com>
2024-01-15 12:50:19 +0530

    1st Commit

---
+++ BloggApp/src/test/java/com/pavan/BloggApp/BloggApp/BloggAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.BloggApp.BloggApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
